30a3a83ad5e4bade613b53661ff471047f504fc6
Marco Zirino <marco@krewmedia.com>
2022-06-12 08:23:14 -0700

    added column template

--- Compare-Rows.Tests.ps1
+++ Compare-Rows.Tests.ps1
@@ -5,7 +5,7 @@ BeforeAll {
 
 Describe 'Compare-Strings' {
 	It 'Can compare strings' {
-		Compare-Rows -Csv1 ./data/outlook/folders.csv -Csv2 ./data/outlook/uniqueRules.csv -columns Name,Name
+		Compare-Rows -Csv1 ./data/outlook/folders.csv -Csv2 ./data/outlook/uniqueRules.csv -columnspec Name
 		# $results.Count | Should -Be 5
 	}
 }
--- Compare-Rows.ps1
+++ Compare-Rows.ps1
@@ -1,10 +1,45 @@
 . ./ComparableString.ps1
 
-function Compare-Rows([string]$csv1, [string]$csv2, [string[]]$columns, [CompareTypes]$method=[CompareTypes]::inherit) {
-	$left = Import-Csv -Path $csv1 | Select @{n = $columns[0]; e = { [ComparableString]::new($_.($columns[0])) } }
-	$right = Import-Csv -Path $csv2 | Select @{n = $columns[1]; e = { [ComparableString]::new($_.($columns[1])) } }
 
+function Get-Column-Spec($template = '$expr') {
+	process {
+		if (($_ -match "(?<field>[a-z]+)(?: as (?<alias>[a-z]+))?$") -eq $false) {
+			throw "Invalid columnspec: ${_}"
+		}
+		$field = $matches.field
+		$alias = $matches.contains('alias') ? $matches.alias : $field
+		$exprString ="`$_.$field" 
+		$code = $template.replace('$expr',$exprString)
+		@{n = $alias; e = [scriptblock]::create($code) }
+	}
+}
+
+function Import-Csv-Column-Spec([string]$path, [string[]] $columnSpec) {
+	Import-Csv -Path $path | Select-Object -Property ($columnSpec | Get-Column-Spec -template '[ComparableString]::new($expr)' )
+}
+
+
+class CompareData {
+	[array]$left;
+	[array]$right;
+	[array]$same;
+
+
+	CompareData([string]$csv1, [string]$csv2, [string[]]$columnSpec) {
+		# choose the "left" elements
+		$this.left = Import-Csv-Column-Spec -path $csv1 $columnSpec
+		$this.right = Import-Csv-Column-Spec -path $csv2 $columnSpec
+	}
+
+	Compare([CompareTypes]$method = [CompareTypes]::equality) {
+		
+	}
+
+}
+
+function Compare-Rows([string]$csv1, [string]$csv2, [string[]]$columnSpec, [CompareTypes]$method = [CompareTypes]::inherit) {
+	$data = [CompareData]::new($csv1, $csv2, $columnSpec)
 	[ComparableString]::MyCompare = $method
-	$results = Compare-Object -IncludeEqual -ReferenceObject $left -DifferenceObject $right -Property Name
+	$results = Compare-Object -IncludeEqual -ReferenceObject $data.left -DifferenceObject $data.right -Property $columns 
 	$results
 }
